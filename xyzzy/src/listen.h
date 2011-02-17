120b3b162b7c96c1f01f9284fbbd136eb0cdad53
katayama <none@none>
2011-02-18 04:49:33 +0900

    use different xyzzysrv_name on unicode build.

--- xyzzy/src/listen.h
+++ xyzzy/src/listen.h
@@ -1,7 +1,11 @@
 #ifndef _listen_h_
 #define _listen_h_
 
+#ifdef UNICODE
+static const TCHAR xyzzysrv_name[] = _T("xyzzy+-server v2");
+#else
 static const TCHAR xyzzysrv_name[] = _T("xyzzy-server v2");
+#endif
 
 // wm_private_xyzzysrv
 //  wparam: process ID
