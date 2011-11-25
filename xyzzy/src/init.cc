56a71ae45a5e94c968f204f6e706c57906c0682b
katayama <none@none>
2011-11-26 06:22:04 +0900

    use common control version 6

--- xyzzy/src/init.cc
+++ xyzzy/src/init.cc
@@ -19,6 +19,14 @@
 # include <crtdbg.h>
 #endif
 
+#pragma comment(linker,                                     \
+                "/manifestdependency:\"type='win32' "       \
+                "name='Microsoft.Windows.Common-Controls' " \
+                "version='6.0.0.0' "                        \
+                "processorArchitecture='*' "                \
+                "publicKeyToken='6595b64144ccf1df' "        \
+                "language='*'\"")
+
 const TCHAR Application::ToplevelClassName[] = _T("Å@");
 const TCHAR Application::FrameClassName[] = _T("  ");
 const TCHAR Application::ClientClassName[] = _T("   ");
