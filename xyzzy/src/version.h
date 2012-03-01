e22f858af9dd30eaa23a1f55fb682a1a1c44c2ac
katayama <none@none>
2012-03-02 06:33:12 +0900

    add '+0' to version string

--- xyzzy/src/version.h
+++ xyzzy/src/version.h
@@ -9,6 +9,8 @@
 # define PROGRAM_MINOR_REVISION 235
 # define PROGRAM_PATCH_LEVEL 0
 
+# define PROGRAM_PLUS_VERSION 0
+
 # define TITLE_BAR_STRING_SIZE 256
 extern TCHAR TitleBarString[];
 extern const TCHAR VersionString[];
